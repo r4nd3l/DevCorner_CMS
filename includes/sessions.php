c403276ac2a3c14b3e351d31c77ee1b72eb26007
matemolnar88 <matemolnar88@gmail.com>
2019-06-26 19:00:38 +0200

    Alert messages update

--- includes/sessions.php
+++ includes/sessions.php
@@ -1,25 +1,28 @@
 <?php
-
   session_start();
-
   function ErrorMessage(){
     if(isset($_SESSION["ErrorMessage"])){
-      $output = "<div class='alert alert-danger'>";
+      $output = '<div class="alert alert-danger alert-dismissible fade show" role="alert">';
       $output .= htmlentities($_SESSION["ErrorMessage"]);
-      $output .= "</div>";
-      $_SESSION["ErrorMessage"] = null;
+      $output .= '<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                  </div>';
+      unset($_SESSION["ErrorMessage"]);
       return $output;
     }
   }
 
   function SuccessMessage(){
     if(isset($_SESSION["SuccessMessage"])){
-      $output = "<div class='alert alert-success'>";
+      $output = '<div class="alert alert-success alert-dismissible fade show" role="alert">';
       $output .= htmlentities($_SESSION["SuccessMessage"]);
-      $output .= "</div>";
-      $_SESSION["SuccessMessage"] = null;
+      $output .= '<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                  </div>';
+      unset($_SESSION["SuccessMessage"]);
       return $output;
     }
   }
-
 ?>
