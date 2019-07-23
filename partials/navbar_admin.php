d203e780465c60795e0b4f9c113b637cd38be0f3
matemolnar88 <matemolnar88@gmail.com>
2019-07-23 23:40:03 +0200

    - Sample images directory added - updated navbar with dropdown-menu

--- partials/navbar_admin.php
+++ partials/navbar_admin.php
@@ -22,16 +22,39 @@
 
       <!-- Login/out part -->
       <ul class="navbar-nav">
-        <li class="nav-item">
-          <div class="btn-group btn-group-sm border-secondary" role="group" aria-label="Basic example">
-            <button type="button" class="btn btn-light border border-secondary">
+        <li class="nav-item ml-2">
+          <div class="btn-group btn-group-sm border-secondary" role="group" aria-label="nav-items">
+            <!-- Home button -->
+            <button type="button" class="btn btn-light border">
               <a href="admin_private.php?a=dashboard" class="text-success"><span class="align-sub"><i class="fas fa-home "></i></span></a>
             </button>
-            <button type="button" class="btn btn-light border border-secondary">
-              <a href="logout.php" class="text-secondary"><span class="align-sub"><i class="fas fa-power-off "></i> Logout</span></a>
-            </button>
+            <!-- Home button - END -->
+
+            <!-- settings button -->
+            <div class="btn-group dropdown">
+              <button class="btn btn-light border border-secondary rounded-right btn-sm" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <a href="" class="text-success"><span class="align-sub"><i class="fas fa-cog"></i></span></a>
+              </button>
+              <div class="dropdown-menu dropdown-menu-right">
+                <a class="dropdown-item text-secondary" href="#"><i class="fas fa-cogs fa-fw text-success mr-1"></i> <span>Settings</span></a>
+                <a class="dropdown-item text-secondary" href="#"><i class="fas fa-language fa-fw text-success mr-1"></i> <span>Language: English (UK)</span></a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item text-secondary" href="#"><i class="far fa-question-circle fa-fw text-success mr-1"></i> <span>Help</span></a>
+                <a class="dropdown-item text-secondary" href="#"><i class="far fa-comment-alt fa-fw text-success mr-1"></i> <span>Send feedback</span></a>
+                <a class="dropdown-item text-secondary" href="#"><i class="fas fa-adjust fa-fw text-success mr-1"></i> <span>Dark theme</span>
+                  <label class="switch float-right">
+                    <input type="checkbox">
+                    <span class="slider round"></span>
+                  </label>
+                </a>
+                <a class="dropdown-item text-secondary" href="logout.php"><i class="fas fa-power-off fa-fw text-success mr-1"></i> <span>Logout</span></a>
+              </div>
+            </div>
+            <!-- settings button - END -->
+
           </div>
         </li>
+
       </ul>
     </div>
 
